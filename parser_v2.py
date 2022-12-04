ed2280041da6f2b74abdd16de901a86c5aeba8aa
jungtop <jungtop@jungtops-MacBook-Pro.local>
2022-12-04 13:11:31 +0530

    updated parser v2

--- parser_v2.py
+++ parser_v2.py
@@ -82,17 +82,8 @@ class LHParser(Alignment):
         main_div = collection_page.find('div',{'id':'content'})
         pecha_links = main_div.select('div.index-container ul li>a')
         for pecha_link in pecha_links:
-            self.parse_page_content(pecha_link.get("href"))
-
-
-    def parse_collection(self,url):
-        collection_page = self.make_request(url)
-        self.collection_name = collection_page.select_one('div#content h1').text
-        self.collection_description = collection_page.select_one("div#content b").text.strip("\n")
-        main_div = collection_page.find('div',{'id':'content'})
-        pecha_links = main_div.select('div.index-container ul li>a')
-        for pecha_link in pecha_links:
             self.parse_page_content(self.start_url+pecha_link.get("href"))
+
 
     def get_lang_code(self,lang):
         code = ""
@@ -122,26 +113,114 @@ class LHParser(Alignment):
     def parse_page_content(self,url):
         page = self.make_request(url)
         source_file = {}
-        base_text = {}
-        has_alignment = []
+        base_texts = []
+        has_alignment = False
         lang_elems = page.select('p#lang-list a')
         language_pages = [[self.get_lang_code(lang_elem.text),lang_elem['href']] for lang_elem in lang_elems]
 
         for language_page in language_pages:
             lang,href = language_page
-            self.extract_page_text(self.start_url+href,lang)
-    
-    def extract_page_text(self,url,lang):
+            base_text,has_alignment = self.extract_page_text(self.start_url+href,lang,has_alignment)
+            base_texts.append(base_text)
+
+        if has_alignment:
+            it = iter(base_texts)
+            the_len = len(next(it))
+            if not all(len(l) == the_len for l in it):
+                raise ValueError('not all lists have same length')
+
+    @staticmethod
+    def remove_endlines(text):
+        prev = ''
+        while prev != text.strip("\n"):
+            prev =text.strip("\n")
+        return prev 
+
+
+    def extract_page_text(self,url,lang,has_alignment):
         base_text = ""
         page = self.make_request(url)
         div_main = page.select_one('div#maintext')
         childrens = div_main.findChildren(recursive=False)
         if len(childrens) == 1 and childrens[0].name == "div":
             childrens = childrens[0].findChildren(recursive=False)
+
+        if lang == "bo":
+            base_text = self.parse_tibetan_page(childrens,has_alignment)
+        else:
+            base_text,has_alignment = self.parse_non_tibetan_page(childrens)    
         
-        for children in childrens
+        return base_text,has_alignment
 
+    def parse_tibetan_page(self,elems,has_alignment):
+        base_text = []
+        for elem in elems:
+            text = self.remove_endlines(elem.get_text())
+            if has_alignment:
+                if not elem.has_attr('class'):
+                    continue
+                if elem['class'][0] in ('HeadingTib','TibetanVerse'):
+                    base_text.append(text)
+            else:
+                base_text.append(text)
+        
+        return base_text
 
+    def extract_text_from_tag(self,elem):
+        base_text = ""
+        texts = list(elem.stripped_strings)
+        for text in texts:
+            base_text+=" "+text
+        clean_text = self.remove_endlines(base_text.strip())
+        return clean_text
+        
+
+    def has_alignmnet(self,elems):
+        has_alignment = False
+        for elem in elems:
+            if not elem.has_attr('class'):
+                continue
+            elif elem['class'][0] in ('HeadingTib','TibetanVerse','TibetanExplanation'):
+                has_alignment=True
+                return has_alignment
+        
+        return has_alignment
+    
+    def parse_aligned_page(self,elems):
+        base_text = []
+        heading = ""
+        skipped_class = ["HeadingTib","TibetanVerse","TibetanExplanation","EnglishPhonetics","Credit","Explanation","Footnote","Bibliography","Bibliographie"]
+        for elem in elems:
+            text = self.extract_text_from_tag(elem)
+            if not elem.has_attr('class'):
+                continue
+            elif elem['class'][0] in skipped_class:
+                continue
+            elif text == "":
+                continue
+            elif elem['class'][0] == "Heading3":
+                heading+=" "+text
+                continue
+            base_text.append(text)
+        
+        if heading:
+            base_text.insert(0,heading)
+        return base_text
+
+    def parse_nonaligned_page(self,elems):
+        base_text = []
+        for elem in elems:
+            base_text.append(elem.text)
+        return base_text
+
+    def parse_non_tibetan_page(self,elems):
+        has_alignment = self.has_alignmnet(elems)
+        if has_alignment:
+            base_text = self.parse_aligned_page(elems)
+        else:
+            base_text = self.parse_nonaligned_page(elems)
+
+        return base_text,has_alignment
 
     def main(self):
         self.pechas_catalog = self.set_up_logger("pechas_catalog")
@@ -169,3 +248,5 @@ class LHParser(Alignment):
 if __name__ == "__main__":
     obj = LHParser("./root")
     obj.main()
+    #obj.parse_page_content("https://www.lotsawahouse.org/tibetan-masters/adeu-rinpoche/white-jambhala")
+    #obj.parse_collection("https://www.lotsawahouse.org/tibetan-masters/dilgo-khyentse/")
